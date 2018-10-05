ff1ca396adeb7300e1fc843351718171d1c9e591
Shelby Solomon Darnell <shelbysdarnel@gmail.com>
2018-10-06 00:00:50 +0300

    Angular stuff

---
+++ TutNetYo/src/app/online.theblacklion.mynetwork.ts
@@ -0,0 +1,27 @@
+import {Asset} from './org.hyperledger.composer.system';
+import {Participant} from './org.hyperledger.composer.system';
+import {Transaction} from './org.hyperledger.composer.system';
+import {Event} from './org.hyperledger.composer.system';
+// export namespace online.theblacklion.mynetwork{
+   export class Commodity extends Asset {
+      tradingSymbol: string;
+      description: string;
+      mainExchange: string;
+      quantity: number;
+      owner: Trader;
+   }
+   export class Trader extends Participant {
+      tradeId: string;
+      firstName: string;
+      lastName: string;
+   }
+   export class Trade extends Transaction {
+      commodity: Commodity;
+      newOwner: Trader;
+   }
+   export class TradeByQuantity extends Transaction {
+      commodity: Commodity;
+      newOwner: Trader;
+      quantity: number;
+   }
+// }
